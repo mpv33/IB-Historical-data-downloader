e3debcee8520a5cdc17f94e9e3b8cbb2ae37c29e
mateshwari verma <46149432+mpv33@users.noreply.github.com>
2020-02-14 10:57:42 +0530

    Update autosaving_data.py

--- autosaving_data.py
+++ autosaving_data.py
@@ -1,10 +1,10 @@
-'''import csv
+import csv
+#method-1
 with open('test.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['sn','name'])
+    writer.writerow(['sn','name'])   # list form each row 
     writer.writerow([1,'mpv'])
-'''
-
+# method-2
 with open(s + '.json', 'a+') as outfile:
     output = '{"date": "' + str(bar.date) + '","open":' + str(bar.open) + ',"high":' + str(
         bar.high) + ',"low":' + str(bar.low) + '' \
